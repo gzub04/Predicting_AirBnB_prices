fa20b27b505f7d500df2d2cdba3963664124ed9c
Mikołaj Jagielski <m.jagielski.3@stud.elka.pw.edu.pl>
2023-06-14 21:41:51 +0200

    Update file main.py

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from sklearn.metrics import mean_squared_error
 import numpy as np
 import pandas as pd
 import glob
+import matplotlib.pyplot as plt
 
 
-NUM_OF_ITERATIONS = 50  # how many times should each ML algorithm be run
+NUM_OF_ITERATIONS = 20  # how many times should each ML algorithm be run
 
 # random forest parameters
 MAX_DEPTH = None
@@ -25,7 +26,16 @@ MIN_SAMPLES_SPLIT_GRADIENT = 10
 LEARNING_RATE_GRADIENT = 0.1
 LOSS_GRADIENT = "squared_error"
 
+dataset_names = []
 
+forest_scores = []
+forest_scores_rms = []
+
+linear_scores = []
+linear_scores_rms = []
+
+gradient_scores = []
+gradient_scores_rms = []
 
 def split_data(dataset):
     x = dataset.iloc[:, 2:].values
@@ -55,9 +65,10 @@ def load_files():
     file_list = glob.glob("Datasets/*.csv")
     dataframes = []
     for file in file_list:
+        dataset_names.append(file.title())
         df = pd.read_csv(file)
         dataframes.append(df)
-    return pd.concat(dataframes, ignore_index=True)
+    return dataframes
 
 
 def run_algorithms(df):
@@ -83,8 +94,56 @@ def run_algorithms(df):
     # print(f'Linear regression score from scikit-learn = {linear_regression.score(x_test, y_test)}')
     # print(f'Random forest RMSE = {root_squared_mean_error(random_forest_regression, x_test, y_test)}')
     # print(f'Linear regression RMSE = {root_squared_mean_error(linear_regression, x_test, y_test)}')
+
+    forest_scores.append(random_forest_regression.score(x_test, y_test))
+    linear_scores.append(linear_regression.score(x_test, y_test))
+    gradient_scores.append(gradient_boosting_regressor.score(x_test, y_test))
+
+    forest_scores_rms.append(root_squared_mean_error(random_forest_regression, x_test, y_test))
+    linear_scores_rms.append(root_squared_mean_error(linear_regression, x_test, y_test))
+    gradient_scores_rms.append(root_squared_mean_error(gradient_boosting_regressor, x_test, y_test))
+
     return [random_forest_regression.score(x_test, y_test), linear_regression.score(x_test, y_test),
             gradient_boosting_regressor.score(x_test, y_test)]
+
+def plotScores():
+    fig, (ax, ax2) = plt.subplots(1, 2, figsize=(8, 8))
+    dataset_indexes = np.arange(len(dataset_names))
+    
+    #plt.xticks(dataset_indexes, dataset_names)
+
+    #ax = fig.add_subplot(211)
+
+    ax.plot(dataset_indexes,forest_scores, "ob", label = "Random Forest")
+    ax.plot( dataset_indexes,linear_scores, "or", label = "Linear Regression")
+    ax.plot( dataset_indexes, gradient_scores, "og", label = "Gradient Boosting")
+    ax.set_xticks(dataset_indexes)
+    ax.set_xticklabels(dataset_names, rotation='vertical')
+    ax.set_title("Score")
+    ax.set_ylim(top = 1.0, bottom = 0)
+    ax.margins(0.2)
+    #plt.xticks(dataset_indexes, dataset_names)
+
+    #ax = fig.add_subplot(211)
+
+    ax2.plot(dataset_indexes,forest_scores_rms, "ob", label = "Random Forest")
+    ax2.plot( dataset_indexes,linear_scores_rms, "or", label = "Linear Regression")
+    ax2.plot( dataset_indexes, gradient_scores_rms, "og", label = "Gradient Boosting")
+    ax2.set_xticks(dataset_indexes)
+    ax2.set_xticklabels(dataset_names, rotation='vertical')
+    ax2.set_title("RMSE")
+    ax2.margins(0.2)
+
+    #plt.ylim(bottom = 0)
+    #plt.xticks(dataset_indexes,dataset_names,rotation='vertical')
+    #plt.legend(loc="upper right")
+    #plt.margins(0.2)
+    #plt.subplots_adjust(bottom=0.5)
+
+    plt.legend(loc="upper right")
+    #plt.margins(0.2)
+    plt.subplots_adjust(bottom=0.5)
+    plt.show()
 
 
 def main():
@@ -96,7 +155,7 @@ def main():
         progress = f"Progress: {i}/{NUM_OF_ITERATIONS}"
         sys.stdout.write('\r' + progress)
         sys.stdout.flush()
-        results = run_algorithms(df)
+        results = run_algorithms(df[i])
         sum_random_forest += results[0]
         sum_linear += results[1]
         sum_gradient_boosting += results[2]
@@ -107,7 +166,10 @@ def main():
     print(f'Average of scores from scikit-learn for Random Forest = {average_random_forest}')
     print(f'Average of scores from scikit-learn for Linear Regression = {average_linear}')
     print(f'Average of scores from scikit-learn for Gradient Boosting = {average_gradient_boosting}')
+    plotScores()
 
 
 if __name__ == '__main__':
     main()
+
+
